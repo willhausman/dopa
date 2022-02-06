94c7559c3c3f0be865cd87d5621ea35aaf63bff7
Will Hausman <will.jhausman@outlook.com>
2022-02-06 14:06:29 -0600

    last tweaks for builtins

--- src/Opa.WebAssembly/OpaPolicy.cs
+++ src/Opa.WebAssembly/OpaPolicy.cs
@@ -7,6 +7,8 @@ public class OpaPolicy : Disposable, IOpaPolicy
     private readonly IOpaRuntime runtime;
     private readonly IOpaSerializer serializer;
 
+    private BuiltinCollection builtins => runtime.Builtins;
+
     public OpaPolicy(IOpaRuntime runtime, IOpaSerializer serializer)
     {
         this.runtime = runtime;
@@ -23,35 +25,20 @@ public class OpaPolicy : Disposable, IOpaPolicy
         throw new NotImplementedException();
     }
 
-    public void AddBuiltin<TResult>(string name, Func<TResult> callback)
-    {
-        var builtin = new Builtin<TResult>(name, runtime, serializer, callback);
-        runtime.Builtins.AddBuiltin(builtin);
-    }
+    public void AddBuiltin<TResult>(string name, Func<TResult> callback) =>
+        builtins.AddBuiltin(new Builtin<TResult>(name, runtime, serializer, callback));
 
-    public void AddBuiltin<TArg, TResult>(string name, Func<TArg, TResult> callback)
-    {
-        var builtin = new Builtin<TArg, TResult>(name, runtime, serializer, callback);
-        runtime.Builtins.AddBuiltin(builtin);
-    }
+    public void AddBuiltin<TArg, TResult>(string name, Func<TArg, TResult> callback) =>
+        builtins.AddBuiltin(new Builtin<TArg, TResult>(name, runtime, serializer, callback));
 
-    public void AddBuiltin<TArg1, TArg2, TResult>(string name, Func<TArg1, TArg2, TResult> callback)
-    {
-        var builtin = new Builtin<TArg1, TArg2, TResult>(name, runtime, serializer, callback);
-        runtime.Builtins.AddBuiltin(builtin);
-    }
+    public void AddBuiltin<TArg1, TArg2, TResult>(string name, Func<TArg1, TArg2, TResult> callback) =>
+        builtins.AddBuiltin(new Builtin<TArg1, TArg2, TResult>(name, runtime, serializer, callback));
 
-    public void AddBuiltin<TArg1, TArg2, TArg3, TResult>(string name, Func<TArg1, TArg2, TArg3, TResult> callback)
-    {
-        var builtin = new Builtin<TArg1, TArg2, TArg3, TResult>(name, runtime, serializer, callback);
-        runtime.Builtins.AddBuiltin(builtin);
-    }
+    public void AddBuiltin<TArg1, TArg2, TArg3, TResult>(string name, Func<TArg1, TArg2, TArg3, TResult> callback) =>
+        builtins.AddBuiltin(new Builtin<TArg1, TArg2, TArg3, TResult>(name, runtime, serializer, callback));
 
-    public void AddBuiltin<TArg1, TArg2, TArg3, TArg4, TResult>(string name, Func<TArg1, TArg2, TArg3, TArg4, TResult> callback)
-    {
-        var builtin = new Builtin<TArg1, TArg2, TArg3, TArg4, TResult>(name, runtime, serializer, callback);
-        runtime.Builtins.AddBuiltin(builtin);
-    }
+    public void AddBuiltin<TArg1, TArg2, TArg3, TArg4, TResult>(string name, Func<TArg1, TArg2, TArg3, TArg4, TResult> callback) =>
+        builtins.AddBuiltin(new Builtin<TArg1, TArg2, TArg3, TArg4, TResult>(name, runtime, serializer, callback));
 
     protected override void DisposeManaged()
     {
