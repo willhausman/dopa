7e0b8b7e47ab9f822f47fc967c9a759a7c80a8e9
Will Hausman <will.jhausman@outlook.com>
2022-08-14 16:23:42 -0500

    Use versioned loader for the rest of the wasmtime tests

--- test/DOPA.Tests/WasmtimeFixture.cs
+++ test/DOPA.Tests/WasmtimeFixture.cs
@@ -7,12 +7,20 @@ public class WasmtimeFixture : Disposable, IRuntimeFixture
 {
     private readonly Lazy<IOpaModule> exampleModule = new Lazy<IOpaModule>(() =>
     {
-        using var stream = OpaLoader.CreateWebAssemblyStream("policies/example.rego", "example/hello");
-        return WasmModule.FromStream("example", stream.Result);
+        using var stream = OpaLoader.CreateWebAssemblyStream("policies/example.rego", "example/hello").Result;
+        return WasmModule.FromStream("example", stream);
     });
     public IOpaModule ExampleModule => exampleModule.Value;
 
-    private readonly Lazy<IOpaModule> builtinsModule = new Lazy<IOpaModule>(() => WasmModule.FromFile("policies/builtins.wasm"));
+    private readonly Lazy<IOpaModule> builtinsModule = new Lazy<IOpaModule>(() =>
+    {
+        var args = new OpaArguments("policies/builtins.rego", "builtins")
+        {
+            Capabilities = "policies/builtins.capabilities.json"
+        };
+        using var stream = OpaLoader.CreateWebAssemblyStream(args).Result;
+        return WasmModule.FromStream("builtins", stream);
+    });
     public IOpaModule BuiltinsModule => builtinsModule.Value;
 
     protected override void DisposeManaged()
